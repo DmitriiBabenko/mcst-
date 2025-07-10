85bc422911d82558067ba52811be3e17ed69a19c
димасик <dimab1559@gmail.com>
2025-07-10 22:29:23 +0300

    add CMakeList

--- main.cpp
+++ main.cpp
@@ -10,24 +10,23 @@ Copyright (c) 2015 Microsoft Corporation
 #include"z3++.h"
 #include<string>
 
-using namespace z3;
 
 void randomFpa32(std::set<std::string>& set, const size_t count) {
-    context ctx;
-    params p(ctx);
+    z3::context ctx;
+    z3::params p(ctx);
     p.set("random_seed", static_cast<unsigned>(time(nullptr)));
-    const tactic t = with(tactic(ctx, "smt"), p);
-    const sort float_ = ctx.fpa_sort(8, 24);
-    const expr a = ctx.constant("a", float_);
-    const expr b = ctx.constant("b", float_);
-    const expr c = ctx.constant("c", float_);
-    solver s = t.mk_solver();
+    const z3::tactic t = with(z3::tactic(ctx, "smt"), p);
+    const z3::sort float_ = ctx.fpa_sort(8, 24);
+    const z3::expr a = ctx.constant("a", float_);
+    const z3::expr b = ctx.constant("b", float_);
+    const z3::expr c = ctx.constant("c", float_);
+    z3::solver s = t.mk_solver();
     s.add(a + b == c);
     s.set(p);
 
-    const expr a_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, a));
-    const expr b_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, b));
-    const expr c_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, c));
+    const z3::expr a_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, a));
+    const z3::expr b_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, b));
+    const z3::expr c_normal = to_expr(ctx, Z3_mk_fpa_is_normal(ctx, c));
 
     s.add(a_normal);
     s.add(b_normal);
@@ -36,8 +35,8 @@ void randomFpa32(std::set<std::string>& set, const size_t count) {
     size_t i = 0;
     do {
         i = set.size();
-        if (s.check() == sat) {
-            model m = s.get_model();
+        if (s.check() == z3::sat) {
+            z3::model m = s.get_model();
             set.insert(m.to_string());
         }
     } while (set.size() > i and i < count);
